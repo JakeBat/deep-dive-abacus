b37e5dac02e7249bfed7403bdd9cecbfe71423b1
JakeBat <jake_batchelor@hotmail.com>
2018-01-23 08:50:35 -0700

    Add formatted output.

--- IdeaProjects/deep-dive-abacus/src/edu/cnm/deepdive/Abacus.java
+++ IdeaProjects/deep-dive-abacus/src/edu/cnm/deepdive/Abacus.java
@@ -1,5 +1,17 @@
 package edu.cnm.deepdive;
 
+import java.util.Arrays;
+
 public class Abacus {
+
+  public static void main(String[] args){
+    double sum = 0;
+    for(String s : args) {
+      sum += Double.parseDouble(s);
+    }
+   // System.out.print("Sum = ");
+    // System.out.println(sum);
+    System.out.printf("Sum of %s = %.2f%n", Arrays.toString(args), sum);
+  }
 
 }
